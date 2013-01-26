7274e1432767317276c417a46032e226b4c7d1dd
Matt Richmond <matt@crowdfavorite.com>
2013-01-25 19:24:41 -0700

    adding ver 1.2

--- fbf_facebook_page_feed.php
+++ fbf_facebook_page_feed.php
@@ -3,10 +3,12 @@
 Plugin Name: FBF - Facebook Page Feed Widget
 Description: Displays your Facebook Page feed in the sidbar of your blog.Simply add your pageID and all your visitors can see your staus!
 Author: Lakshmanan PHP
-Version: 1.0
+Version: 1.2
 */
 
-/*
+/* 
+   Version 1.0 - update - Showing avatar, shor code, widget styles 25-aug-2012
+   
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2, 
     as published by the Free Software Foundation.
@@ -16,7 +18,6 @@ Version: 1.0
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 */
-
 
 // Display Facebook messages
 function fbf_facebook_messages($options) {
@@ -30,66 +31,88 @@ function fbf_facebook_messages($options) {
 	if (!is_numeric($options['num']) or $options['num']<=0) {
 		return __('Number of status not valid','fbf');
 	}
+                 
+	$link_target = ($options['link_target_blank']) ? ' target="_blank" ' : ''; // Checking Link target - Open new windo/tab or in same window
+	
+	$avatar_size = ($options['avatar_size']) ? $options['avatar_size'] : 'small'; // avatar_size	
+	
 
 
-        
-                  
-$link_target = ($options['link_target_blank']) ? ' target="_blank" ' : '';
-	// Checking Link target - Open new windo/tab or in same window
-	
-	
-	
-	// Credits http://www.kaylaknight.com/reading-a-facebook-page-rss-feed-with-php/
-	 // Without this "ini_set" Facebook's RSS url is all screwy for reading!
-    // This is the most essential line, don't forget it.
-    ini_set('user_agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9');
+ // Fetching feed using wordpress // Credits http://digwp.com/2011/09/feed-display-multiple-columns/
  
-    // This URL is the URL to the Facebook Page's RSS feed.
-    // Go to the page's profile, and on the left-hand side click "Get Updates vis RSS"
-    $rssUrl = "http://www.facebook.com/feeds/page.php?id=".$options['pageID']."&format=rss20";
-    $xml = simplexml_load_file($rssUrl); // Load the XML file
- 
-    // This creates an array called "entry" that puts each <item> in FB's
-    // XML format into the array
-    $entry = $xml->channel->item;
- 
-    // This is just a blank string I create to add to as I loop through our
-    // FB feed. Feel free to format however you want, or do whatever else
-	/*
+	include_once(ABSPATH . WPINC . '/feed.php');
+	
+	 $optionID = $options['pageID']; // Getting Facebook Page ID 
+	 $rss_array = explode(',',$optionID);
+	 $feed_link_array = "";
+	
+	 foreach($rss_array as $feed_link_id){ // Creating string of RSS links seperated by commas
+		 $feed_link_array .= ',http://www.facebook.com/feeds/page.php?id='.$feed_link_id.'&format=rss20';	
+	 }
+	
+	 if($feed_link_array{0}==",")
+		$feed_link_array=substr($feed_link_array,1,strlen($feed_link_array));    
+	 $feed_links = explode(",",$feed_link_array); // Array of RSS Feed links
+	 
+	 if(function_exists('fetch_feed')) {
+			$feed = fetch_feed($feed_links);
+			if (!is_wp_error($feed)) : $feed->init();
+				$feed->set_output_encoding('UTF-8');	// set encoding
+				$feed->handle_content_type();		// ensure encoding
+				$feed->set_cache_duration(21600);	// six hours in seconds
+				$limit = $feed->get_item_quantity($options['num']);	// get  feed items
+				$items = $feed->get_items(0, $limit);	// set array
+			endif;
+	}
+	
+	if ($limit == 0) { 
+		echo '<p>RSS Feed currently unavailable.</p>'; 
+	} else {
 
-	*/
-    // you want with the data.
     $returnMarkup = '';
-	$returnMarkup .= '<ul class="fbf_facebook_page_widget">';
-   
-    for ($i = 0; $i < $options['num']; $i++) {
-        $returnMarkup .= "<li><img class=facebook_page-avatar src=http://graph.facebook.com/".$options['pageID']."/picture?type=small width=40 height=40 alt=".$entry[$i]->author." /><a href=".$entry[$i]->link." class='facebook_page-link' ".$link_target.">".substr($entry[$i]->title,0,200)."</a>"; // Title of the update
-       // $returnMarkup .= "<p>".$entry[$i]->link."</p>"; // Link to the update
-    if ($options['show_description'] != '') {   
-	   $returnMarkup .= "<br><span>".$entry[$i]->description."</span>"; // Full content
-    }
-	 //   $returnMarkup .= "<p>".$entry[$i]->pubDate."</p>"; // The date published
+	$returnMarkup .= '<div class="fbf_facebook_page_widget_container">	
+<ul class="fbf_facebook_page_widget">';
+	
+	$blocks = array_slice($items, 0, $options['num']);
+	foreach ($blocks as $block) {  
 		
+		if ($options['feed_title'] == "true" ) { 
+			$feedtitle ="<h4><a href=".$block->get_permalink()." class='facebook_page-link' ".$link_target.">".substr($block->get_title(), 0, 200)."</a></h4>"; // Title of the update
+		}elseif ($options['feed_title'] == "" ) {
+			$feedtitle = null;
+		}
 	
-	 if ($options['update'] != '') {
-		$time = strtotime($entry[$i]->pubDate);
-		$tval = timesince($time);
-			$h_time = ( ( abs( time() - $time) ) < 86400 ) ? sprintf( __('%s ago', 'fbf'), human_time_diff( $time )) : date(__('Y/m/d'), $time);
-			$returnMarkup .= ', '.sprintf( __('%s', 'fbf'),' <span class="facebook_page-timestamp"><abbr title="' . date(__('Y/m/d H:i:s', 'fbf'), $time) . '">' . $tval . '</abbr></span>' );
-	} 
+		$returnMarkup .= $feedtitle;	
 	
-	//  Show Timestamp - if option enabled
+		# Shows avatar of Facebook page	
+			if ($options['show_avatar'] != '') { 
+			$returnMarkup .="<div class=\"facebook_page-avatar\"><img src=\"http://graph.facebook.com/".$options['pageID']."/picture?type=".$avatar_size."\"  alt=".$block->author." /></div>";
+			}
+		
+			if ($options['show_description'] != '') {   
+				$desc_feed = str_replace('href="http://www.facebook.com', 'href="', $block->get_description()); // Emptying all links
+				$desc = str_replace('href="', 'href="http://www.facebook.com', $desc_feed); // adding facebook link - to avoid facebook redirector l.php's broken link error
+				$returnMarkup .= "<div class=\"fbf_desc\">".$desc."</div>"; // Full content
+			}
+		
+			 if ($options['update'] != '') {
+				$time = strtotime($block->get_date("l, F jS, Y"));
+				$tval = timesince($time);
+					$h_time = ( ( abs( time() - $time) ) < 86400 ) ? sprintf( __('%s ago', 'rstw'), human_time_diff( $time )) : date(__('Y/m/d'), $time);
+					$returnMarkup .= ', '.sprintf( __('%s', 'rstw'),' <span class="facebook_page-timestamp"><abbr title="' . date(__('Y/m/d H:i:s', 'rstw'), $time) . '">' . $tval . '</abbr></span>' );
+			 } //  Show Timestamp - if option enabled
 		 $returnMarkup .='</li>';
-		
-      //  $returnMarkup .= "<p>".$entry[$i]->author."</p></li>"; // The author of the post
-    }
- 
-    // Finally, we return (or in this case echo) our formatted string with our
-    // Facebook page feed data in it!
-    // $returnMarkup;
+	} // For Loop Ends here
 	
+	$returnMarkup .='</ul>
+		</div>';
+	}
+
 	return $returnMarkup;
 }
+
+
+
   // Formatting Time stamps
 function timesince($original) {
     // array of time period chunks
@@ -159,18 +182,33 @@ class FacebookPageFeedWidget extends WP_Widget {
 				'label'	=> __( 'Show # of Posts', 'fbf' ),
 				'type'	=> 'text'
 			),
-			
+			array(
+				'name'	=> 'avatar_size',
+				'label'	=> __( 'Avatar size', 'fbf' ),
+				'type'	=> 'radio',
+				'values' => array('square'=>'Square','small'=>'Small','normal'=>'Normal','large'=>'Large')
+			),			
 			array(
 				'name'	=> 'update',
 				'label'	=> __( 'Show timestamps', 'fbf' ),
 				'type'	=> 'checkbox'
 			),
-			
+			array(
+				'name'	=> 'feed_title',
+				'label'	=> __( 'Show feed title', 'fbf' ),
+				'type'	=> 'checkbox'
+			),
 			array(
 				'name'	=> 'show_description',
 				'label'	=> __( 'Show Description', 'fbf' ),
 				'type'	=> 'checkbox'
 			),
+			array(
+				'name'	=> 'show_avatar',
+				'label'	=> __( 'Show Avatar', 'fbf' ),
+				'type'	=> 'checkbox'
+			),
+			
 			array(
 				'name'	=> 'link_target_blank',
 				'label'	=> __( 'Create links on new window / tab', 'fbf' ),
@@ -203,21 +241,27 @@ class FacebookPageFeedWidget extends WP_Widget {
 				$instance[$val['name']] = strip_tags($new_instance[$val['name']]);
 			} else if ($val['type']=='checkbox') {
 				$instance[$val['name']] = ($new_instance[$val['name']]=='on') ? true : false;
+			} else if ($val['type']=='radio') {
+				$instance[$val['name']] = $new_instance[$val['name']];
 			}
+			
 		}
         return $instance;
     }
 
     /** @see WP_Widget::form */
     function form($instance) {
-		if (empty($instance)) {
-			$instance['title']			= __( 'FBF Facebook page Feed Widget', 'fbf' );
-			$instance['pageID']		= '';
-			$instance['num']			= '5';
-			$instance['update']			= true;
-			$instance['show_description']	= true;
-			$instance['link_target_blank']	= true;
-		}					
+		$default['title']			= __( 'FBF Facebook page Feed Widget', 'fbf' );
+		$default['pageID']		= '';
+		$default['num']			= '5';
+		$default['update']			= true;
+		$default['show_description']	= true;
+		$default['show_avatar']	= true;
+		$default['avatar_size']	= 'small'; // Available sizes square, small, normal, or large
+		$default['link_target_blank']	= true;
+		$default['feed_title'] = true;
+	
+		$instance = wp_parse_args($instance,$default);
 	
 		foreach ($this->options as $val) {
 			$label = '<label for="'.$this->get_field_id($val['name']).'">'.$val['label'].'</label>';
@@ -227,6 +271,14 @@ class FacebookPageFeedWidget extends WP_Widget {
 			} else if ($val['type']=='checkbox') {
 				$checked = ($instance[$val['name']]) ? 'checked="checked"' : '';
 				echo '<input id="'.$this->get_field_id($val['name']).'" name="'.$this->get_field_name($val['name']).'" type="checkbox" '.$checked.' /> '.$label.'<br />';
+			} else if ($val['type']=='radio') {
+				echo '<p>'.$label.'<br />';
+				foreach($val['values'] as $key=>$name){
+					$label = '<label for="'.$this->get_field_id($val['name'].'_'.$key).'">'.$name.'</label>';
+					$checked = ($instance[$val['name']] == $key) ? 'checked="checked"' : '';
+					echo '<input id="'.$this->get_field_id($val['name'].'_'.$key).'" name="'.$this->get_field_name($val['name']).'" type="radio" '.$checked.' value="'.$key.'" />'.$label.'&nbsp;';
+				}
+				echo '<br/><br/>';
 			}
 		}
 	}
@@ -235,3 +287,33 @@ class FacebookPageFeedWidget extends WP_Widget {
 
 // register FacebookPageFeedWidget widget
 add_action('widgets_init', create_function('', 'return register_widget("FacebookPageFeedWidget");'));
+
+// register stylesheet 25-aug-2012
+add_action('wp_head', 'fbf_add_header_css', 100);
+function fbf_add_header_css() {
+	echo '<link type="text/css" media="screen" rel="stylesheet" href="' . plugins_url('fbf-facebook-page-feed-widget/fbf_facebook_page_feed.css') . '" />' . "\n";
+}
+
+
+// Short code FacebookPageFeed 25-aug-2012
+function fbf_short_code($atts) {
+   	 $atts['pageID'] = $atts['pageid'];
+	 $atts= shortcode_atts(array(
+			'pageID' => '33138223345',
+			'num' => '5',
+			'update' => false,
+			'show_description' => false,
+			'show_avatar' => false,
+			'avatar_size' => 'small',
+			'link_target_blank' => false,
+			'feed_title' => true,
+     ), $atts);
+    
+	 return fbf_facebook_messages($atts);
+}
+// sample short code
+// [fbf_page_feed pageID="33138223345" num="2" show_description="true" update="true" show_avatar="true" avatar_size="square" link_target_blank="true" feed_title => "true" ]
+
+add_shortcode('fbf_page_feed', 'fbf_short_code');
+
+
